6d845686112bb9f61d2bb9c1fae25e1974ea2026
Your Name <you@example.com>
2024-11-09 23:40:37 +0530

    msg

--- variable.tf
+++ variable.tf
@@ -26,7 +26,7 @@ variable "private_subnet_cidrs" {
 
 variable "database_subnet_cidrs" {
     default = ["10.0.21.0/24", "10.0.22.0/24"]
-}
-variable "is_peering_required"{
-    default =true
-}
+ }
+#variable "is_peering_required"{
+#     default =true
+#}

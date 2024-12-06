31fd0d87ab25a8bc457637fa9650f8d0c5f1a36f
Your Name <you@example.com>
2024-12-06 15:05:00 +0530

    msg

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
     value = module.vpc.vpc_id
 }
-output "az_info" {
-    value = module.vpc.az_info
-}
+# output "az_info" {
+#     value = module.vpc.az_info
+# }
 
 # output "default_vpc_info" {
 #     value = module.vpc.default_vpc_info
